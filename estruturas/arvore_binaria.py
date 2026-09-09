d0deeb47e198ad12bbd731efcd8b21c8311c23b8
LanaMoraes06 <161388805+LanaMoraes06@users.noreply.github.com>
2026-09-09 13:24:00 -0300

    feat: adicionar funções de inserção, busca, exclusão e ordem

--- estruturas/arvore_binaria.py
+++ estruturas/arvore_binaria.py
@@ -1,19 +1,83 @@
-class NoArvore:
-    def __init__(self, codigo, posicao):
-        self.esquerda = None
-        self.direita = None
-        self.codigo = codigo
-        self.posicaio = posicao
+class No:
+    def __init__(self, cod, pos):
+        self.cod = int(cod)
+        self.pos = int(pos)
+        self.esq = None
+        self.dir = None
 
+class ArvoreBinaria:
+    def __init__(self):
+        self.raiz = None
 
-def adicionar_no_arvore(raiz, codigo, posicao):
-    if raiz is None:
-        return NoArvore(codigo, posicao)
-    if int(codigo) > raiz.codigo:
-        raiz.direita = adicionar_no_arvore(raiz.direita, codigo, posicao)
-    else:
-        raiz.esquerda = adicionar_no_arvore(raiz.direita, codigo, posicao)
-    return raiz
+    def inserir_no(self, cod, pos):
+        cod = int(cod)
+        pos = int(pos)
+        if self.raiz is None:
+            self.raiz = No(cod, pos)
+        else: 
+            self.inserir_no_filho(self.raiz, cod, pos)
+        
+    def inserir_no_filho(self, pai, cod, pos):
+        if cod < pai.cod: 
+            if pai.esq is None:
+                pai.esq = No(cod, pos)
+            else: 
+                self.inserir_no_filho(pai.esq, cod, pos)
+        elif cod > pai.cod:
+            if pai.dir is None:
+                pai.dir = No(cod, pos)
+            else:
+                self.inserir_no_filho(pai.dir, cod, pos)
+        else:
+            pai.pos = pos
 
-
+    def buscar(self, cod):
+        return self.buscar_cod(self.raiz, int(cod))
     
+    def buscar_cod(self, raiz, cod):
+        if raiz is None:
+            return None
+        elif raiz.cod == cod:
+            return raiz.pos
+        elif cod < raiz.cod:
+            return self.buscar_cod(raiz.esq, cod)
+        else:
+            return self.buscar_cod(raiz.dir, cod)
+
+    def deletar(self, cod):
+        self.raiz = self.excluir(self.raiz, int(cod))
+        
+    def excluir(self, raiz, cod):
+        if raiz is None:
+            return None
+        if cod < raiz.cod:
+            raiz.esq = self.excluir(raiz.esq, cod)
+        elif cod > raiz.cod:
+            raiz.dir = self.excluir(raiz.dir, cod)
+        else:
+            if raiz.esq is None:
+                return raiz.dir
+            elif raiz.dir is None:
+                return raiz.esq
+            else:
+                ex = self.minimo(raiz.dir)
+                raiz.cod = ex.cod
+                raiz.pos = ex.pos
+                raiz.dir = self.excluir(raiz.dir, ex.cod)
+        return raiz
+        
+    def minimo(self, pai):
+        atual = pai
+        while atual.esq is not None:
+            atual = atual.esq
+        return atual
+    
+    def obter_posicoes_em_ordem(self):
+        posicoes = []
+        def percorrer(no):
+            if no is not None:
+                percorrer(no.esq)
+                posicoes.append(no.pos)
+                percorrer(no.dir)
+        percorrer(self.raiz)
+        return posicoes
