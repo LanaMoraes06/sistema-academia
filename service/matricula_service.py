48f07fa3632aa3fa10354f368aeb860a0d786691
LanaMoraes06 <161388805+LanaMoraes06@users.noreply.github.com>
2026-09-18 11:02:24 -0300

    feat: implementar service de matricula

---
+++ service/matricula_service.py
@@ -0,0 +1,49 @@
+from model.entidades import Matricula, Aluno, Modalidade
+
+class Matricula:
+    def __init__(self, gerenciador_mat, gerenciador_aluno, gerenciador_mod):
+        self.gerenciador_mat = gerenciador_mat
+        self.gerenciador_aluno = gerenciador_aluno
+        self.gerenciador_mod = gerenciador_mod
+
+    def realizar_matricula(self, codigo_mat, codigo_aluno, codigo_mod, qtd_aulas):
+        if self.gerenciador_mat.findById(codigo_mat) is not None:
+            raise ValueError("Erro: O código de matricula já existe!")    
+        if self.gerenciador_aluno.findById(codigo_aluno) is not None:
+            raise ValueError(f"Erro: Aluno com o {codigo_aluno} não existe")
+        
+        modalidade = self.gerenciador_mod.findById(codigo_mod)
+        if modalidade is None:
+            raise ValueError("Erro: Modalidade não encontrada")
+
+
+        if modalidade.total_alunos >= modalidade.limite_alunos:
+            raise ValueError(f"Erro: A modalidade {modalidade.descricao} já atingiu o limite de {modalidade.limite_alunos} alunos")
+        
+        nova_matricula = Matricula(codigo_mat, codigo_aluno, codigo_mod, qtd_aulas)
+        self.gerenciador_mat.save(nova_matricula)
+
+        modalidade.total_alunos += 1
+        self.gerenciador_mod.update(modalidade)
+
+    def listar_matriculas(self):
+        matricula = self.gerenciador_mat.findAll()
+        if not matricula:
+            raise ValueError(f"Não existem matriculas cadastrados no sistema.")
+        return matricula
+
+    def buscar_cod(self, codigo_mat):
+        matricula = self.gerenciador_prof.findById(codigo) 
+        if matricula is None:
+            raise ValueError(f"Erro: Nenhuma matricula encontrado no sistema com o código: {codigo_mat}")
+        return matricula
+
+    def excluir(self, codigo_mat):
+        self.buscar_cod(codigo_mat)
+        self.gerenciador_mat.delete(codigo_mat)
+
+    def atualizar(self, codigo_mat, codigo_aluno, codigo_mod, qtd_aulas):
+        self.buscar_cod(codigo_mat)
+        mat_modificada = Matricula(codigo_mat, codigo_aluno, codigo_mod, qtd_aulas)
+        self.gerenciador_mat.update(mat_modificada)
+
